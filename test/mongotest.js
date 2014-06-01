898343fa0bde3ec58499d26488a13ffbba3788a6
Jim Fingal <jim.fingal@gmail.com>
2014-05-31 20:59:17 -0700

    POC from accessing mongohq from localhost

--- test/mongotest.js
+++ test/mongotest.js
@@ -1,5 +1,17 @@
-var mongohelper = require('../app/mongohelper');
+var MongoClient = require('mongodb').MongoClient;
+var db_name = "lahack";
+var TWEET_COLLECTION = "tweets"
 
-var insertDoc = mongohelper.insertDocIntoCollection('test_collection');
+var connection = process.env.LOCAL_MONGOHQ
 
-insertDoc({'name' : 'this is a test doc using the partial'});
+var MongoClient = require('mongodb').MongoClient;
+
+
+MongoClient.connect(connection, function(err, db) {
+  if(err) throw err;
+  db.collection(TWEET_COLLECTION).count(function(err, count) {
+      if(err) throw err;
+      console.log(count);
+  });
+});
+
